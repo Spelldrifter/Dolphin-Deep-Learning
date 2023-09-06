ab80f182dcf1910083740e75035b5104ccffbd61
Spelldrifter <spelldrifter@proton.me>
2023-09-06 17:07:07 +0000

    Fix circle.yml

--- rbm/Rbm.cpp
+++ rbm/Rbm.cpp
@@ -21,3 +21,33 @@ Rbm::Rbm(float momentum, float alpha, int visibleSize, int hiddenSize){
   vb = (float*)mkl_malloc(sizeof(float) * visibleSize, 64);
   memset(b, 0, sizeof(float) * visibleSize);
   memset(vb, 0, sizeof(float) * visibleSize);
+  c = (float*)mkl_malloc(sizeof(float) * hiddenSize, 64);
+  vc = (float*)mkl_malloc(sizeof(float) * hiddenSize, 64);
+  memset(c, 0, sizeof(float) * hiddenSize);
+  memset(vc, 0, sizeof(float) * hiddenSize);
+  random = (float*)mkl_malloc(sizeof(float) * (hiddenSize >= visibleSize ? hiddenSize:visibleSize), 64);
+  int max = hiddenSize >= visibleSize ? hiddenSize : visibleSize;
+  for(int i = 0; i < max; i++){
+    random[i] = rand() / RAND_MAX;
+  }
+}
+
+Rbm::~Rbm(){
+  mkl_free(W);
+  mkl_free(vW);
+  mkl_free(b);
+  mkl_free(vb);
+  mkl_free(c);
+  mkl_free(vc);
+  mkl_free(random);
+}
+float Rbm::computeCostAndGradient(float* &data, int batchSize){
+  float error = 0.0;  //v1 = data
+  //h1 = sigmrnd(v1*W'+c) batchSize * hiddenSize
+  /*float* h1 = (float*)mkl_malloc(sizeof(float) * batchSize * hiddenSize, 64);
+  memset(h1, 0, sizeof(float) * batchSize * hiddenSize);*/
+  //printf("h1:%p\n",h1);
+  //printf("before the first matrix\n");
+  //printf("1\n");
+  /*cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
+		batchSize, hiddenSize, visibleSize,
