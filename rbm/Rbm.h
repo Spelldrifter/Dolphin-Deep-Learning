65ca436b3c6af72d70438c68204e563869818e2f
Spelldrifter <spelldrifter@proton.me>
2023-09-14 19:25:19 +0000

    tex

--- rbm/Rbm.h
+++ rbm/Rbm.h
@@ -6,4 +6,5 @@ class Rbm{
     Rbm(float momentum, float alpha, int visibleSize, int hiddenSize);
     Rbm(const Rbm &rbm);
     float computeCostAndGradient(float* &data, int batchSize);//data batchSize*visibleSize
-  
+    void updateWeight();
+    void train(f
